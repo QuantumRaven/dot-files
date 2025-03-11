a8cff0ddee3be4e8cce0d7681884dfb9199473c6
QuantumRaven <carpenterrchloe@protonmail.com>
2025-03-11 11:05:42 -0400

    add fzf and path items at EOF

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Add paths to PATH variable
+fish_add_path ~/.local/bin
+
+# Initiate Starship config for fish shell
+
+starship init fish | source
+
+# Set up fzf key bindings for fish shell
+
+fzf --fish | source
