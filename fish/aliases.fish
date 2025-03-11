c51eb84908fe88120a50b4cd19ec1f6b17d1c7dc
QuantumRaven <carpenterrchloe@protonmail.com>
2025-03-11 12:38:14 -0400

    sync aliases from home dir

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,40 +1,40 @@
 # ansible - Config management
-alias ap-inv = "ansible-playbook -i $1"
+alias ap-inv="ansible-playbook -i $1"
 
 # asciinema - Record terminal
-alias a-play = "asciinema play $1"
-alias a-rec = "asciinema rec $1"
-alias a-upload = "asciinema upload $1"
+alias a-play="asciinema play $1"
+alias a-rec="asciinema rec $1"
+alias a-upload="asciinema upload $1"
 
 # git
-alias gh = "history | grep"
+alias gh="history | grep"
 
 # grep
-alias grep = "grep -iE --color=auto"
+alias grep="grep -iE --color=auto"
 
 # lsblk - list block devices
-alias lbk = "lsblk -o NAME,FSTYPE,LABEL,UUID,MOUNTPOINTS "
+alias lbk="lsblk -o NAME,FSTYPE,LABEL,UUID,MOUNTPOINTS "
 
 # lsd - list directories and contents
-alias l = "lsd -l"
-alias la = "lsd -a"
-alias ll = "lsd -lha"
+alias l="lsd -l"
+alias la="lsd -a"
+alias ll="lsd -lha"
 
 # rsync
-alias rsync = "rsync -avhP"
-alias s-rsync = "sudo rsync -avhP"
+alias rsync="rsync -avhP"
+alias s-rsync="sudo rsync -avhP"
 
 # systemctl - suspend device
-alias sus = "systemctl suspend"
+alias sus="systemctl suspend"
 
 # wf-recorder - Record desktop via CLI
-alias wfreca = "wf-recorder -a -f $1"
-alias wfrec = "wf-recorder -f $1"
+alias wfreca="wf-recorder -a -f $1"
+alias wfrec="wf-recorder -f $1"
 
 # zellij
-alias zel-a = "zellij attach"
-alias zel-da = "zellij delete-all-sessions"
-alias zel-d = "zellij delete-session"
-alias zel-l = "zellij list-sessions"
-alias zel-n = "zellij -s"
-alias zel-set = "zellij setup"
+alias zel-a="zellij attach"
+alias zel-da="zellij delete-all-sessions"
+alias zel-d="zellij delete-session"
+alias zel-l="zellij list-sessions"
+alias zel-n="zellij -s"
+alias zel-set="zellij setup"
