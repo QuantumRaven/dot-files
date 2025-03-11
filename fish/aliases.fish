7d5c5182c47686f4bba894bfa3cdbe44967b0975
QuantumRaven <carpenterrchloe@protonmail.com>
2025-03-11 12:04:17 -0400

    add aliases for fuzzy finder

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,19 +1,41 @@
+# ansible - Config management
 alias ap-inv="ansible-playbook -i $1"
+
+# asciinema - Record terminal
 alias a-play="asciinema play $1"
 alias a-rec="asciinema rec $1"
 alias a-upload="asciinema upload $1"
-alias dusize="du -sh * | sort -hr"
+
+# fzf - Fuzzy Finder
+alias fuzzy-nvim="nvim $(fzf --preview=bat {} --reverse)"
+alias sfuzzy-nvim="nvim $(fzf --preview=bat {} --reverse)"
+
+# git
 alias gh="history | grep"
+
+# grep
 alias grep="grep -iE --color=auto"
+
+# lsblk - list block devices
 alias lbk="lsblk -o NAME,FSTYPE,LABEL,UUID,MOUNTPOINTS "
+
+# lsd - list directories and contents
 alias l="lsd -l"
 alias la="lsd -a"
 alias ll="lsd -lha"
+
+# rsync
 alias rsync="rsync -avhP"
 alias s-rsync="sudo rsync -avhP"
+
+# systemctl - suspend device
 alias sus="systemctl suspend"
+
+# wf-recorder - Record desktop via CLI
 alias wfreca="wf-recorder -a -f $1"
 alias wfrec="wf-recorder -f $1"
+
+# zellij
 alias zel-a="zellij attach"
 alias zel-da="zellij delete-all-sessions"
 alias zel-d="zellij delete-session"
