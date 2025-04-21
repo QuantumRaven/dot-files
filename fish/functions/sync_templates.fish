40d33767449b9db8fa8f29d10c08d76140388343
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-21 15:08:08 -0400

    Add comments to file

--- fish/functions/sync_templates.fish
+++ fish/functions/sync_templates.fish
@@ -1,18 +1,31 @@
 function sync_templates
+    
+    # Set source where rsync will pull from and cd into dir.
 
     set -x SOURCE "/home/quantumcrow/workspace/github_projects/quantumraven/template-files/"
 
     cd "$SOURCE"
 
+    # List contents of SOURCE dir.
+    # Using fd in place of lsd for filenames only.
+
     fd
+    
+    # Change from SOURCE dir back to current working dir.
 
     cd -
+
+    # Prompt user to pick a file name and assign it to FILE variable
 
     echo -e "\nChoose file: "
     read FILE
 
+    # Prompt user to pick where to store and what to name the file and assign it to the DEST variable.
+
     echo -e "\nWhere to sync file: "
     read DEST
+
+    # Use rsync -avhP (aliased as just rsync) to synchronize the file from SOURCE/FILE to the designated DEST.
 
     rsync "$SOURCE""$FILE" "$DEST"
 
