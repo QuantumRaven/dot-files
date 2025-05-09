9b07659852a38d36aa83942e81d9449f8f52b201
QuantumRaven <carpenterrchloe@protonmail.com>
2025-05-09 17:43:20 -0400

    Add ability to create new file
    If the file to be worked on is selected in fzf interface, it opens that
    file name. If the file to be worked on does not exist, press the "ESC"
    key and it will prompt the user for the path to the new file, then
    create it in Neovim.

--- fish/functions/vfuzzy.fish
+++ fish/functions/vfuzzy.fish
@@ -1,5 +1,13 @@
 function vfuzzy
 
-  fd -H -E .git -t f | fzf --preview "bat {}" --bind "enter:execute(nvim {})" --reverse
+  set select_file (fd -H -E .git -t f | fzf --preview "bat {}" --bind "enter:execute(nvim {})" --reverse)
+
+  if test -n "$select_file"
+    nvim "$select_file"
+  else
+    echo "No file selected. Enter new file name: "
+    read new_file
+    nvim "$new_file"
+  end
   
 end
