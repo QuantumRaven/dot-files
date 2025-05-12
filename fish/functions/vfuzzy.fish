70cdf95de2d85103278bd916aa36322bfab38f19
QuantumRaven <carpenterrchloe@protonmail.com>
2025-05-12 11:33:11 -0400

    Return to previous state
    Remove additional if/else statements from function.

--- fish/functions/vfuzzy.fish
+++ fish/functions/vfuzzy.fish
@@ -1,13 +1,5 @@
 function vfuzzy
 
-  set select_file (fd -H -E .git -t f | fzf --preview "bat {}" --bind "enter:execute(nvim {})" --reverse)
-
-  if test -n "$select_file"
-    nvim "$select_file"
-  else
-    echo "No file selected. Enter new file name: "
-    read new_file
-    nvim "$new_file"
-  end
+  fd -H -E .git -t f | fzf --preview "bat {}" --bind "enter:execute(nvim {})" --reverse
   
 end
