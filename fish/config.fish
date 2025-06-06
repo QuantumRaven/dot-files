876abcaf2054cdd8302300d0fa7c1478e595208f
QuantumRaven <quantumncorvidae@proton.me>
2025-06-06 07:17:56 -0400

    Fix: Remove alias reference
    Aliases have been replaced with functions.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-. ~/.config/fish/aliases.fish
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -7,7 +5,6 @@ end
 # Add paths to PATH variable
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/go/bin
-fish_add_path $HOME/.local/bin/pastel_v0.10.0
 
 # Initiate Starship config for fish shell
 
