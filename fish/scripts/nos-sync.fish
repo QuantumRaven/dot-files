cf2db471e3ef7c962a5d7d16f66f264374c1b993
QuantumRaven <quantumncorvidae@proton.me>
2025-06-29 01:28:34 -0400

    refactor: Remove nos-sync.fish

--- fish/scripts/nos-sync.fish
+++
@@ -1,4 +0,0 @@
-function nos-sync
-	command sudo rsync -avhP ~/workspace/github_projects/quantumraven/nixos/hosts/andromeda/configuration.nix /etc/nixos/
-	command rsync -avhP ~/workspace/github_projects/quantumraven/nixos/modules /etc/nixos/
-end
